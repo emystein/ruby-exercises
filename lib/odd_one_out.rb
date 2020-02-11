5975310d209eed1b52256ced5482ab3c9f87d825
Emiliano Menéndez <emystein@gmail.com>
2020-02-11 20:37:55 -0300

    What will be the odd one out? kata

--- lib/odd_one_out.rb
+++ lib/odd_one_out.rb
@@ -1,40 +1,14 @@
 #https://www.codewars.com/kata/55b080eabb080cd6f8000035
-def odd_one_out(word)
-    word.chars.uniq
-    .map{|c| position_and_count(c, word)}
-    .select{|p| p.count.odd?}
-    .sort_by{|p| p.position}
-    .map{|p| p.char}
-end
+def odd_one_out(s)
+  odds = Hash.new
 
-def position_and_count(char, word)
-    position = 0
-    count = 0
-
-    p = 0
-    word.chars.each do |c|
-        if c == char
-            position = p
-            count = count + 1
-        end
-        p = p + 1
+  s.chars.each do |c|
+    if odds.has_key?(c)
+      odds.delete(c)
+    else
+      odds[c] = true
     end
+  end
 
-    PositionAndCount.new(char, position, count)
-end
-
-class PositionAndCount
-    attr_reader :char, :position, :count
-
-    def initialize(char, position, count)
-        @char = char
-        @position = position
-        @count = count
-    end
-
-    def ==(other)
-        self.char == other.char &&
-        self.position == other.position &&
-        self.count == other.count
-    end
+  odds.keys
 end
--- spec/odd_one_out_spec.rb
+++ spec/odd_one_out_spec.rb
@@ -1,24 +1,16 @@
 require "odd_one_out"
 
-describe "character last position and count" do
-    it "register unique position and count 1" do
-        expect(position_and_count("H", "Hello World")).to eq PositionAndCount.new("H", 0, 1)
-    end
-
-    it "register last position and count 2" do
-        expect(position_and_count("o", "Hello World")).to eq PositionAndCount.new("o", 7, 2)
-    end
-
-    it "register last position and count 3" do
-        expect(position_and_count("l", "Hello World")).to eq PositionAndCount.new("l", 9, 3)
-    end
-end
-
 describe "odd one out" do
-   it "leaves no out in empty string" do
-       expect(odd_one_out("")).to eq []
-    end
-    it "leaves out odd characters in 'Hellow World'" do
-        expect(odd_one_out("Hello World")).to eq ["H", "e", " ", "W", "r", "l", "d"]
-   end
+  it "leaves no out in empty string" do
+    expect(odd_one_out("")).to eq []
+  end
+  it "leaves out odd characters in non-empty string" do
+    expect(odd_one_out("Hello World")).to eq ["H", "e", " ", "W", "r", "l", "d"]
+    expect(odd_one_out("Codewars")).to eq ["C", "o", "d", "e", "w", "a", "r", "s"]
+    expect(odd_one_out("woowee")).to eq []
+    expect(odd_one_out("wwoooowweeee")).to eq []
+    expect(odd_one_out("racecar")).to eq ["e"]
+    expect(odd_one_out("Mamma")).to eq ["M"]
+    expect(odd_one_out("Mama")).to eq ["M", "m"]
+  end
 end
