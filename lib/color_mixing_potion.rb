a325b680e8149fbb735b084e57c3b8419fa2eb90
Emiliano Menéndez <emystein@gmail.com>
2020-02-14 18:56:51 -0300

    WIP: Mix Potions with color and volume kata

---
+++ lib/color_mixing_potion.rb
@@ -0,0 +1,41 @@
+class ColorComponent
+    attr_accessor :color_code, :volume
+
+    def initialize(color_code, volume)
+        @color_code = color_code
+        @volume = volume
+    end
+
+    def mix(other)
+        new_volume = @volume + other.volume
+
+        color_value = ((1 + @color_code) * @volume) - 1
+        other_color_value = ((1 + other.color_code) * other.volume) - 1
+
+        new_color_value = [255, ((color_value + other_color_value) / new_volume)].min
+
+        ColorComponent.new(new_color_value, new_volume)
+    end
+
+    def ==(other)
+        @color_code == other.color_code && @volume == other.volume
+    end
+end
+
+class Potion
+    attr_reader :color_components, :volume
+  
+    def initialize(color, volume)
+        @color_components = color.map{|c| ColorComponent.new(c, volume)}
+        @volume = volume
+    end
+    
+    def color
+        @color_components.map{|c| c.color_code}.to_a
+    end
+
+    def mix(other)
+        mixed_colors = @color_components.zip(other.color_components).map{|c1, c2| c1.mix(c2)}.map{|c| c.color_code}.to_a
+        Potion.new(mixed_colors, @volume + other.volume)
+    end
+end
---
+++ spec/color_mixing_potion_spec.rb
@@ -0,0 +1,31 @@
+require "color_mixing_potion"
+
+describe "color component mix" do
+  it "mix color 0 with volume 10 and color 0 with volume 5" do
+    color_component1 = ColorComponent.new(0, 10)
+    color_component2 = ColorComponent.new(0, 5)
+
+    expect(color_component1.mix(color_component2)).to eq ColorComponent.new(0, 15)
+  end
+  it "mix color 255 with volume 10 and color 0 with volume 5" do
+    color_component1 = ColorComponent.new(255, 10)
+    color_component2 = ColorComponent.new(0, 5)
+
+    expect(color_component1.mix(color_component2)).to eq ColorComponent.new(170, 15)
+  end
+  it "mix color 255 with volume 7 and color 0 with volume 12" do
+    color_component1 = ColorComponent.new(255, 7)
+    color_component2 = ColorComponent.new(51, 12)
+
+    expect(color_component1.mix(color_component2)).to eq ColorComponent.new(127, 19)
+  end
+end
+
+describe "Potion mix" do
+  it "mix two Potions" do
+    potion1 = Potion.new([153, 210, 199], 32)
+    potion2 = Potion.new([135,  34,   0], 17)
+    mix_potion = potion1.mix(potion2)
+    expect(mix_potion.color).to eq [147, 149, 130]
+  end
+end
