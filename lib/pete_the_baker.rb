c92de2af13450166cdf0a0ecb9b5b5f971087c86
Emystein <emystein@gmail.com>
2020-02-17 13:02:48 -0300

    Pete the baker kata using collect

--- lib/pete_the_baker.rb
+++ lib/pete_the_baker.rb
@@ -1,20 +1,4 @@
 # https://www.codewars.com/kata/525c65e51bf619685c000059
 def cakes(recipe, available)
-    recipe.keys.each do |ingredient|
-        if !available.has_key?(ingredient) || available[ingredient] < recipe[ingredient]
-            return 0
-        end
-    end
-
-    return 1 + cakes(recipe, consume_ingredients(recipe, available))
-end
-
-def consume_ingredients(recipe, available)
-    new_available = available.dup
-
-    recipe.keys.each do |ingredient|
-        new_available[ingredient] -= recipe[ingredient]
-    end
-
-    new_available
+  recipe.collect { | k, v | available[k].to_i / v }.min
 end
