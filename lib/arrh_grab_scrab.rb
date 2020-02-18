3fe99da6c3b3c3b7be5c069acfd5165345c89ade
Emystein <emystein@gmail.com>
2020-02-17 22:59:35 -0300

    Deleted repeated anagrams kata

--- lib/arrh_grab_scrab.rb
+++
@@ -1,5 +0,0 @@
-# https://www.codewars.com/kata/52b305bec65ea40fe90007a7
-def grabscrab(anagram, dictionary)
-    sorted_anagram = anagram.chars.sort
-    dictionary.select{|w| w.chars.sort == sorted_anagram}
-end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -4,4 +4,7 @@ describe "Anagrams" do
     it "find anagrams of abba" do
         expect(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada'])).to eq ['aabb', 'bbaa']
     end
+    it 'filter all out' do
+        expect(anagrams("ourf", ["one","two","three"])).to eq []
+    end
 end
