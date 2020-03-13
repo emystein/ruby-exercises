6c2cf97622ea4b31b8c29f454aee172b97606de5
Emiliano Menéndez <emystein@gmail.com>
2020-03-13 00:10:55 -0300

    first non-consecutive number kata third pass

--- lib/first_non_consecutive.rb
+++ lib/first_non_consecutive.rb
@@ -1,4 +1,4 @@
 # https://www.codewars.com/kata/58f8a3a27a5c28d92e000144
 def first_non_consecutive(arr)
-    arr.select.with_index{ |e, i| (i.between?(1, arr.length) && arr[i] != arr[i - 1] + 1) }.first || nil
+    arr.each_cons(2) { |a, b| return b if b - a > 1 }
 end
