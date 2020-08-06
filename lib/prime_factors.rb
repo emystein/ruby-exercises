b4f15bdc1de1d587cc4767d48a8beab9df3229a5
Emiliano Menéndez <emystein@gmail.com>
2020-08-06 20:34:09 -0300

    Formatted code of prime factors

--- lib/prime_factors.rb
+++ lib/prime_factors.rb
@@ -1,7 +1,7 @@
-# https://www.codewars.com/kata/54d512e62a5e54c96200019e
 require 'prime'
 
-def primeFactors(n)
-    factors = n.prime_division
-    factors.map{|p, e| e == 1 ? "(#{p})" : "(#{p}**#{e})"}.join
+# https://www.codewars.com/kata/54d512e62a5e54c96200019e
+def prime_factors(number)
+  factors = number.prime_division
+  factors.map { |p, e| e == 1 ? "(#{p})" : "(#{p}**#{e})" }.join
 end
--- spec/prime_factors_spec.rb
+++ spec/prime_factors_spec.rb
@@ -1,22 +1,22 @@
 require 'prime_factors'
 
-describe "Primes in numbers" do
-    it "factors out 2 as (2)" do
-        expect(primeFactors(2)).to eq '(2)'
-    end
-    it "factors out 4 as (2**2)" do
-        expect(primeFactors(4)).to eq '(2**2)'
-    end
-    it "factors out 6 as (2)(3)" do
-        expect(primeFactors(6)).to eq '(2)(3)'
-    end
-    it "factors out 8 as (2**3)" do
-        expect(primeFactors(8)).to eq '(2**3)'
-    end
-    it "factors out 12 as (2**2)(3)" do
-        expect(primeFactors(12)).to eq '(2**2)(3)'
-    end
-    it "factors out 36 as (2**2)(3**2)" do
-        expect(primeFactors(36)).to eq '(2**2)(3**2)'
-    end
+describe 'Primes in numbers' do
+  it 'factors out 2 as (2)' do
+    expect(prime_factors(2)).to eq '(2)'
+  end
+  it 'factors out 4 as (2**2)' do
+    expect(prime_factors(4)).to eq '(2**2)'
+  end
+  it 'factors out 6 as (2)(3)' do
+    expect(prime_factors(6)).to eq '(2)(3)'
+  end
+  it 'factors out 8 as (2**3)' do
+    expect(prime_factors(8)).to eq '(2**3)'
+  end
+  it 'factors out 12 as (2**2)(3)' do
+    expect(prime_factors(12)).to eq '(2**2)(3)'
+  end
+  it 'factors out 36 as (2**2)(3**2)' do
+    expect(prime_factors(36)).to eq '(2**2)(3**2)'
+  end
 end
