76a5cc72a6e0427f0ad6b400f43c517a9e036113
Emiliano Menéndez <emystein@gmail.com>
2020-08-06 21:25:21 -0300

    Formatted code of split_strings

--- spec/split_strings_spec.rb
+++ spec/split_strings_spec.rb
@@ -1,16 +1,16 @@
 require 'split_strings'
 
-describe "Split Strings" do
-    it "split empty string should be empty array" do
-        expect(solution('')).to eq [] 
-    end
-    it "split abc should be ab, c_" do
-        expect(solution('abc')).to eq ['ab', 'c_'] 
-    end
-    it "split abcd should be ab, cd" do
-        expect(solution('abcd')).to eq ['ab', 'cd'] 
-    end
-    it "split abcdef should be ab, cd, ef" do
-        expect(solution('abcdef')).to eq ['ab', 'cd', 'ef'] 
-    end
+describe 'Split Strings' do
+  it 'split empty string should be empty array' do
+    expect(solution('')).to eq []
+  end
+  it 'split abc should be ab, c_' do
+    expect(solution('abc')).to eq %w[ab c_]
+  end
+  it 'split abcd should be ab, cd' do
+    expect(solution('abcd')).to eq %w[ab cd]
+  end
+  it 'split abcdef should be ab, cd, ef' do
+    expect(solution('abcdef')).to eq %w[ab cd ef]
+  end
 end
