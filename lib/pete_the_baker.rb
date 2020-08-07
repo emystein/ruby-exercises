a375e54ae1bd312172a29941f620ab06cfb2725c
Emiliano Menéndez <emystein@gmail.com>
2020-08-06 21:19:53 -0300

    Formatted code of pete_the_baker

--- lib/pete_the_baker.rb
+++ lib/pete_the_baker.rb
@@ -1,4 +1,4 @@
 # https://www.codewars.com/kata/525c65e51bf619685c000059
 def cakes(recipe, available)
-  recipe.collect { | k, v | available[k].to_i / v }.min
+  recipe.collect { |k, v| available[k].to_i / v }.min
 end
