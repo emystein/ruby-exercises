eb00c993f9c437e83edb44442c030f397346c465
Emiliano Menéndez <emystein@gmail.com>
2020-08-06 21:16:01 -0300

    Formatted code of first_non_consecutive

--- lib/first_non_consecutive.rb
+++ lib/first_non_consecutive.rb
@@ -1,4 +1,4 @@
 # https://www.codewars.com/kata/58f8a3a27a5c28d92e000144
-def first_non_consecutive(arr)
-    arr.each_cons(2) { |a, b| return b if b - a > 1 }
+def first_non_consecutive(array)
+  array.each_cons(2) { |a, b| return b if b - a > 1 }
 end
