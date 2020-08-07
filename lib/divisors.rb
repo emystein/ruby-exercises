3ac4e1893bbaedfd1a2e2242a2f2706d9170e204
Emiliano Menéndez <emystein@gmail.com>
2020-08-06 21:10:08 -0300

    Formatted code of divisors

--- lib/divisors.rb
+++ lib/divisors.rb
@@ -1,5 +1,5 @@
 # https://www.codewars.com/kata/544aed4c4a30184e960010f4
-def divisors(n)
-  divs = (2..(n / 2)).select { |d| n.modulo(d) == 0 }
-  divs.empty? ? "#{n} is prime" : divs
+def divisors(number)
+  divs = (2..(number / 2)).select { |d| number.modulo(d).zero? }
+  divs.empty? ? "#{number} is prime" : divs
 end
