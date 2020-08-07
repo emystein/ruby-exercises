e7ee54cb21f064c4b94e700e8e40bfa4335e7fe8
Emiliano Menéndez <emystein@gmail.com>
2020-08-06 21:09:03 -0300

    Formatted code of digital_root

--- lib/digital_root.rb
+++ lib/digital_root.rb
@@ -1,4 +1,4 @@
 # https://www.codewars.com/kata/541c8630095125aba6000c00
-def digital_root(n)
-    n < 10 ? n : digital_root(n.digits.sum)
+def digital_root(number)
+  number < 10 ? number : digital_root(number.digits.sum)
 end
