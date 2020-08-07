1997cab76700eb7b1dd3d9fc61ebff2f433a085d
Emiliano Menéndez <emystein@gmail.com>
2020-08-06 21:08:06 -0300

    Formatted code of complete_the_pattern_1

--- lib/complete_the_pattern_1.rb
+++ lib/complete_the_pattern_1.rb
@@ -1,4 +1,4 @@
 # https://www.codewars.com/kata/5572f7c346eb58ae9c000047
-def pattern(n)
-    (1..n).map{|x| x.to_s * x}.join("\n")
+def pattern(number)
+  (1..number).map { |x| x.to_s * x }.join("\n")
 end
