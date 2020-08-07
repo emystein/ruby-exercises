6e8021dfc313713357c292dc112891549e83fba1
Emiliano Menéndez <emystein@gmail.com>
2020-08-06 21:10:53 -0300

    Formatted code of duplicate_count

--- lib/duplicate_count.rb
+++ lib/duplicate_count.rb
@@ -1,6 +1,6 @@
 # https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1
 def duplicate_count(text)
-    counts = Hash.new(0)
-    text.downcase.chars.each{|c| counts[c] += 1}
-    counts.values.count{|v| v > 1}
+  counts = Hash.new(0)
+  text.downcase.chars.each { |c| counts[c] += 1 }
+  counts.values.count { |v| v > 1 }
 end
--- spec/duplicate_count_spec.rb
+++ spec/duplicate_count_spec.rb
@@ -1,13 +1,13 @@
 require 'duplicate_count'
 
-describe "Duplicate Count" do
-   it "abcde counts 0" do
-       expect(duplicate_count('abcde')).to eq 0
-   end
-   it "abcdeaa counts 1" do
-       expect(duplicate_count('abcdeaa')).to eq 1
-   end
-   it "abcdeaB counts 2" do
-       expect(duplicate_count('abcdeaB')).to eq 2
-   end
+describe 'Duplicate Count' do
+  it 'abcde counts 0' do
+    expect(duplicate_count('abcde')).to eq 0
+  end
+  it 'abcdeaa counts 1' do
+    expect(duplicate_count('abcdeaa')).to eq 1
+  end
+  it 'abcdeaB counts 2' do
+    expect(duplicate_count('abcdeaB')).to eq 2
+  end
 end
