7c112907f5ba49564b78c2d380bd0551120f8253
Emiliano Menéndez <emystein@gmail.com>
2020-08-06 21:00:52 -0300

    Formatted code of basic_math_on_string

--- lib/basic_math_on_string.rb
+++ lib/basic_math_on_string.rb
@@ -1,5 +1,5 @@
 # https://www.codewars.com/kata/5809b62808ad92e31b000031
-def calculate(str)
-    numbers_with_sign = str.gsub(/plus|minus/, 'plus' => '+', 'minus' => '-')
-    eval(numbers_with_sign).to_s
+def calculate(string)
+  numbers_with_sign = string.gsub(/plus|minus/, 'plus' => '+', 'minus' => '-')
+  eval(numbers_with_sign).to_s
 end
