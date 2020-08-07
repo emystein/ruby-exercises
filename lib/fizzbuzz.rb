2af05cfa3d16270a2d4781531d9ed6edc541d885
Emiliano Menéndez <emystein@gmail.com>
2020-08-06 21:16:41 -0300

    Formatted code of fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(number)
-    result = multiple_to_string(number, 3, 'fizz')
-    result += multiple_to_string(number, 5, 'buzz')
-    result.empty? ? number.to_s : result
+  result = multiple_to_string(number, 3, 'fizz')
+  result += multiple_to_string(number, 5, 'buzz')
+  result.empty? ? number.to_s : result
 end
 
 def multiple_to_string(multiple, number, string)
-    multiple.modulo(number) == 0 ? string : ''
+  multiple.modulo(number).zero? ? string : ''
 end
