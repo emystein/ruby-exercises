b7be4fab80cfe424ddc415ebf37c4e75ff91acb9
Emiliano Menéndez <emystein@gmail.com>
2021-01-15 16:14:41 -0300

    Parameterized top 3 words

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem "rspec", "~> 3.9"
+gem 'rspec-parameterized'
 
 gem 'solargraph', group: :development
 
--- spec/kyu4/top_3_words_spec.rb
+++ spec/kyu4/top_3_words_spec.rb
@@ -1,23 +1,29 @@
 require 'kyu4/top_3_words'
+require 'rspec-parameterized'
 
 describe 'WordCount' do
-  it 'count words in single line text' do
-    expect(WordCount.new('a').by_word).to eq({ 'a' => 1 })
-    expect(WordCount.new('a b').by_word).to eq({ 'a' => 1, 'b' => 1 })
-    expect(WordCount.new('a a').by_word).to eq({ 'a' => 2 })
-    expect(WordCount.new('a a b').by_word).to eq({ 'a' => 2, 'b' => 1 })
-    expect(WordCount.new('a a b b').by_word).to eq({ 'a' => 2, 'b' => 2 })
+  where(:text, :count_by_word, :top_three) do
+    [
+      ['a', { 'a' => 1 }, %w[a]],
+      ['a b', { 'a' => 1, 'b' => 1 }, %w[b a]],
+      ['a a', { 'a' => 2 }, %w[a]],
+      ['a a b', { 'a' => 2, 'b' => 1 }, %w[a b]],
+      ['a b b', { 'a' => 1, 'b' => 2 }, %w[b a]],
+      ['a a b b', { 'a' => 2, 'b' => 2 }, %w[b a]],
+      ['a b c', { 'a' => 1, 'b' => 1, 'c' => 1 }, %w[c b a]],
+      ['a a b c', { 'a' => 2, 'b' => 1, 'c' => 1 }, %w[a c b]],
+      ['a b b c', { 'a' => 1, 'b' => 2, 'c' => 1 }, %w[b c a]],
+      ['a b c c', { 'a' => 1, 'b' => 1, 'c' => 2 }, %w[c b a]]
+    ]
   end
-  it 'sorted' do
-    expect(WordCount.new('a a a b b c').sorted).to eq(%w[c b a])
-  end
-  it 'top 3 words' do
-    expect(WordCount.new('a').top(3)).to eq ['a']
-    expect(WordCount.new('a b').top(3)).to eq %w[b a]
-    expect(WordCount.new('a b c').top(3)).to eq %w[c b a]
-    expect(WordCount.new('a a b c').top(3)).to eq %w[a c b]
-    expect(WordCount.new('a b b c').top(3)).to eq %w[b c a]
-    expect(WordCount.new('a b c c').top(3)).to eq %w[c b a]
+
+  with_them do
+    it 'count words in single line text' do
+      expect(WordCount.new(text).by_word).to eq count_by_word
+    end
+    it 'top 3 words' do
+      expect(WordCount.new(text).top(3)).to eq top_three
+    end
   end
 end
 
