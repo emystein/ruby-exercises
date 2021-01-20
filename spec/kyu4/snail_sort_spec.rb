f1c63c1cf78bc2890da9e96517e9cbf64b33c082
Emiliano Menéndez <emystein@gmail.com>
2021-01-20 20:45:22 -0300

    Snail sort 4x4 matrix test

--- spec/kyu4/snail_sort_spec.rb
+++ spec/kyu4/snail_sort_spec.rb
@@ -21,6 +21,11 @@ describe 'SquareMatrix.snail_sorted' do
 
     expect(matrix.snail_sorted).to eq([1, 2, 3, 6, 9, 8, 7, 4, 5])
   end
+  it 'snail snort 4 x 4 matrix' do
+    matrix = SquareMatrix.new([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
+
+    expect(matrix.snail_sorted).to eq([1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10])
+  end
 end
 
 describe 'SquareMatrix.reduce' do
