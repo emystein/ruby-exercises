eccb9838acb2726f668bbbed99f261e30d83bb65
Emiliano Menéndez <emystein@gmail.com>
2021-02-15 23:13:41 -0300

    String mix.

--- lib/kyu4/string_mix.rb
+++ lib/kyu4/string_mix.rb
@@ -1,38 +1,37 @@
 # https://www.codewars.com/kata/5629db57620258aa9d000014
 
-class StringStats
+def string_mix(string1, string2)
+  compare = StringCompare.new
+
+  result = compare.diff_with_min_occurrences(string1, string2, 2)
+
+  result.sort_by(LetterRepetitionsDescendingOrder.new)
+        .format(JoinStringNumberAndLetterRepetitions.new)
+end
+
+class LowercaseStringStats
   def initialize(string)
-    @string = string
-    count
+    @letter_count = string.chars
+                           .filter { |c| c != ' ' }
+                           .filter { |c| c.match(/[[:lower:]]/) }
+                           .group_by(&:downcase)
+                           .transform_values(&:size)
   end
 
   def count
-    @letter_count = @string.chars
-                           .filter { |c| c != ' ' }
-                           .group_by(&:downcase)
-                           .transform_values(&:size)
-
     @letter_count
   end
 
   def letter_count(letter)
-    @letter_count[letter]
+    @letter_count[letter] ||= 0
   end
 
   def letters
     @letter_count.keys
   end
 
-  def count_with_min(min)
-    @string.chars
-           .filter { |c| c != ' ' }
-           .group_by(&:downcase)
-           .transform_values(&:size)
-           .filter { |_k, v| v >= min }
-  end
-
   def letters_diff(other_stats)
-    common_letters = letters.intersection(other_stats.letters)
+    common_letters = letters.union(other_stats.letters)
 
     letter_diff_factory = LetterOccurrenceDiffFactory.new(self, other_stats)
 
@@ -42,8 +41,8 @@ end
 
 class StringCompare
   def diff_with_min_occurrences(string1, string2, min_occurrences)
-    string1_stats = StringStats.new(string1)
-    string2_stats = StringStats.new(string2)
+    string1_stats = LowercaseStringStats.new(string1)
+    string2_stats = LowercaseStringStats.new(string2)
 
     letters_diff = string1_stats.letters_diff(string2_stats)
 
@@ -60,6 +59,11 @@ class StringDiff
     @letter_diffs.map(&:letter)
   end
 
+  def sort_by(order_to_apply)
+    @letter_diffs = order_to_apply.sort(@letter_diffs)
+    self
+  end
+
   def format(format_to_apply)
     format_to_apply.format(@letter_diffs)
   end
@@ -73,10 +77,10 @@ class LetterOccurrenceDiffFactory
 
   def for(letter)
     if @string1_stats.letter_count(letter) > @string2_stats.letter_count(letter)
-      string_number = 1
+      string_number = '1'
       letter_count = @string1_stats.letter_count(letter)
     elsif @string1_stats.letter_count(letter) < @string2_stats.letter_count(letter)
-      string_number = 2
+      string_number = '2'
       letter_count = @string2_stats.letter_count(letter)
     else
       string_number = '='
@@ -87,14 +91,34 @@ class LetterOccurrenceDiffFactory
   end
 end
 
-LetterOccurrenceDiff = Struct.new(:string_number, :letter, :occurrences)
+LetterOccurrenceDiff = Struct.new(:string_number, :letter, :occurrences) {
+  def format(format_to_apply)
+    format_to_apply.format(self)
+  end
+}
+
+class JoinStringNumberAndLetterRepetitions
+  def format(elements)
+    elements.map { |element| element.format(StringNumberAndLetterRepetitions.new) }.join('/')
+  end
+end
 
 class StringNumberAndLetterRepetitions
-  def format(diff)
-    sorted(diff).map { |d| "#{d.string_number}:#{d.letter * d.occurrences}" }.join('/')
+  def format(letter_diff)
+    "#{letter_diff.string_number}:#{letter_diff.letter * letter_diff.occurrences}"
   end
+end
 
-  def sorted(diff)
-    diff.sort { |a, b| a.occurrences <=> b.occurrences }.reverse
+class LetterRepetitionsDescendingOrder
+  def sort(diff)
+    by_occurrences = diff.group_by(&:occurrences)
+
+    occurrences_desc = by_occurrences.keys.sort.reverse
+
+    occurrences_desc.flat_map do |occurrences|
+      by_occurrences[occurrences].sort_by do |letter_diff|
+        letter_diff.format(StringNumberAndLetterRepetitions.new)
+      end
+    end
   end
 end
--- spec/kyu4/string_mix_spec.rb
+++ spec/kyu4/string_mix_spec.rb
@@ -1,16 +1,11 @@
 require 'kyu4/string_mix'
 require 'rspec-parameterized'
 
-describe 'StringStats' do
+describe 'LowercaseStringStats' do
   it 'count by letter' do
-    stats = StringStats.new('Are they here')
+    stats = LowercaseStringStats.new('Are they here')
 
-    expect(stats.count).to eq({'a' => 1, 'r' => 2, 'e' => 4, 't' => 1, 'h' => 2, 'y' => 1})
-  end
-  it 'count by letter with minimum occurrences' do
-    stats = StringStats.new('Are they here')
-
-    expect(stats.count_with_min(2)).to eq({'r' => 2, 'e' => 4, 'h' => 2})
+    expect(stats.count).to eq({'r' => 2, 'e' => 4, 't' => 1, 'h' => 2, 'y' => 1})
   end
 end
 
@@ -27,6 +22,24 @@ describe 'StringCompare' do
 
     result = compare.diff_with_min_occurrences('Are they here', 'yes, they are here', 2)
 
-    expect(result.format(StringNumberAndLetterRepetitions.new)).to eq '2:eeeee/2:yy/=:hh/=:rr'
+    expect(result.sort_by(LetterRepetitionsDescendingOrder.new).format(JoinStringNumberAndLetterRepetitions.new)).to eq '2:eeeee/2:yy/=:hh/=:rr'
+  end
+end
+
+describe 'string_mix' do
+  where(:string1, :string2, :expected) do
+    [
+      ['looping is fun but dangerous', 'less dangerous than coding', '1:ooo/1:uuu/2:sss/=:nnn/1:ii/2:aa/2:dd/2:ee/=:gg'],
+      ['In many languages', "there's a pair of functions", '1:aaa/1:nnn/1:gg/2:ee/2:ff/2:ii/2:oo/2:rr/2:ss/2:tt'],
+      ['Lords of the Fallen', 'gamekult', '1:ee/1:ll/1:oo'],
+      ['codewars', 'codewars', ''],
+      ['A generation must confront the looming ', 'codewarrs', '1:nnnnn/1:ooooo/1:tttt/1:eee/1:gg/1:ii/1:mm/=:rr']
+    ]
+  end
+
+  with_them do
+    it 'mix' do
+      expect(string_mix(string1, string2)).to eq(expected)
+    end
   end
 end
