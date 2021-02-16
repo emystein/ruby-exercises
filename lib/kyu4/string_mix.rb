65d5820686b10d3a445b72e1ed73f130e72017d8
Emiliano Menéndez <emystein@gmail.com>
2021-02-15 21:00:07 -0300

    WIP: String mix.

--- lib/kyu4/string_mix.rb
+++ lib/kyu4/string_mix.rb
@@ -1,15 +1,115 @@
 # https://www.codewars.com/kata/5629db57620258aa9d000014
 
-class StringAnalysis
+class StringStats
   def initialize(string)
     @string = string
+    count
+  end
+
+  def count
+    @letter_count = @string.chars
+                           .filter { |c| c != ' ' }
+                           .group_by(&:downcase)
+                           .transform_values(&:size)
+
+    @letter_count
+  end
+
+  def letter_count(letter)
+    @letter_count[letter]
+  end
+
+  def letters
+    @letter_count.keys
   end
 
   def count_with_min(min)
     @string.chars
-            .filter{|c| c != ' '}
-            .group_by{ |c| c.downcase}
-            .transform_values{|v| v.size}
-            .filter{|k, v| v >= min}
+           .filter { |c| c != ' ' }
+           .group_by(&:downcase)
+           .transform_values(&:size)
+           .filter { |_k, v| v >= min }
+  end
+
+  def insersect(stats_to_intersect)
+    StringStatsIntersection.new(self, stats_to_intersect)
+  end
+end
+
+class StringStatsIntersection
+  def initialize(stats1, stats2)
+    @common_letters = stats1.letters.intersection(stats2.letters)
+    @letter_diff_factory = LetterDiffFactory.new(stats1, stats2)
+  end
+
+  def letters_diff
+    @common_letters.map { |letter| @letter_diff_factory.for(letter) }
+  end
+end
+
+class StringCompare
+  def diff_with_min_occurrences(string1, string2, min_occurrences)
+    string1_stats = StringStats.new(string1)
+    string2_stats = StringStats.new(string2)
+
+    letters_diff = string1_stats.insersect(string2_stats).letters_diff
+
+    StringDiff.new(letters_diff.filter { |letter_diff| letter_diff.occurrences >= min_occurrences })
+  end
+end
+
+class StringDiff
+  def initialize(diff)
+    @letter_diffs = diff
+  end
+
+  def letters
+    @letter_diffs.map(&:letter)
+  end
+
+  def format(format_to_apply)
+    format_to_apply.format(@letter_diffs)
+  end
+end
+
+class LetterDiffFactory
+  def initialize(string1_stats, string2_stats)
+    @string1_stats = string1_stats
+    @string2_stats = string2_stats
+  end
+
+  def for(letter)
+    if @string1_stats.letter_count(letter) > @string2_stats.letter_count(letter)
+      string_number = 1
+      letter_count = @string1_stats.letter_count(letter)
+    elsif @string1_stats.letter_count(letter) < @string2_stats.letter_count(letter)
+      string_number = 2
+      letter_count = @string2_stats.letter_count(letter)
+    else
+      string_number = '='
+      letter_count = @string1_stats.letter_count(letter)
+    end
+
+    LetterDiff.new(string_number, letter, letter_count)
+  end
+end
+
+class LetterDiff
+  attr_reader :string_number, :letter, :occurrences
+
+  def initialize(string_number, letter, occurrences)
+    @string_number = string_number
+    @letter = letter
+    @occurrences = occurrences
+  end
+end
+
+class StringNumberAndLetterRepetitionsFormat
+  def format(diff)
+    sorted(diff).map { |d| "#{d.string_number}:#{d.letter * d.occurrences}" }.join('/')
+  end
+
+  def sorted(diff)
+    diff.sort { |a, b| a.occurrences <=> b.occurrences }.reverse
   end
 end
--- spec/kyu4/string_mix_spec.rb
+++ spec/kyu4/string_mix_spec.rb
@@ -1,10 +1,32 @@
 require 'kyu4/string_mix'
 require 'rspec-parameterized'
 
-describe 'StringAnalysis' do
-  it 'count by letter with minimum occurrences' do
-    analysis = StringAnalysis.new('Are they here')
+describe 'StringStats' do
+  it 'count by letter' do
+    stats = StringStats.new('Are they here')
 
-    expect(analysis.count_with_min(2)).to eq({'r' => 2, 'e' => 4, 'h' => 2})
+    expect(stats.count).to eq({'a' => 1, 'r' => 2, 'e' => 4, 't' => 1, 'h' => 2, 'y' => 1})
+  end
+  it 'count by letter with minimum occurrences' do
+    stats = StringStats.new('Are they here')
+
+    expect(stats.count_with_min(2)).to eq({'r' => 2, 'e' => 4, 'h' => 2})
+  end
+end
+
+describe 'StringCompare' do
+  it 'get letters in common between strings' do
+    compare = StringCompare.new
+
+    result = compare.diff_with_min_occurrences('Are they here', 'yes, they are here', 2)
+
+    expect(result.letters).=~ %w[r e h y]
+  end
+  it 'format letters and occurrences' do
+    compare = StringCompare.new
+
+    result = compare.diff_with_min_occurrences('Are they here', 'yes, they are here', 2)
+
+    expect(result.format(StringNumberAndLetterRepetitionsFormat.new)).to eq '2:eeeee/2:yy/=:hh/=:rr'
   end
 end
